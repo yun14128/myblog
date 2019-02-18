cd0f95a22586518f24f5d7950860115138bc1015
yun14128 <yun14128>
2019-02-18 15:33:13 +0800

    demo init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            // { text: 'Home', link: '/' },
+            { text: 'Guide', link: 'https://www.baidu.com/s?wd=%E9%98%BF%E8%B1%86%E7%89%9B%E9%80%BC&rsv_spt=1&rsv_iqid=0xaecc99130009f052&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=16&rsv_sug1=9&rsv_sug7=101&rsv_t=81e6A1hj%2FmoDTu1%2FlbNZgNjXKCeKUsE%2FFn7wOS0mbE6u5nJ9SVAfWP8BWxp48LVXYSt8&rsv_sug2=0&inputT=3580&rsv_sug4=3580' },
+            { text: 'External', link: 'http://caibaojian.com/vuepress/guide/' }
+        ]
+        // sidebar: [
+        //     '/index',
+        //     ['/index', 'Explicit link text']
+        // ]
+    }
+}
